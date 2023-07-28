e1d9f2dab938ca40ce0bd185e363ccd06c142abe
ventasminetronix <minetronix.control@gmail.com>
2023-07-28 09:54:42 -0500

    proyecto examen

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+
+import Dashboard from "views/Dashboard.js";
+
+import Notifications from "views/Notifications.js";
+
+import UserProfile from "views/UserProfile.js";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    rtlName: "لوحة القيادة",
+    icon: "tim-icons icon-chart-pie-36",
+    component: <Dashboard />,
+    layout: "/admin",
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    rtlName: "إخطارات",
+    icon: "tim-icons icon-bell-55",
+    component: <Notifications />,
+    layout: "/admin",
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    rtlName: "ملف تعريفي للمستخدم",
+    icon: "tim-icons icon-single-02",
+    component: <UserProfile />,
+    layout: "/admin",
+  },
+];
+export default routes;
